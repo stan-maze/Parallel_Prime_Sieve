3c0c8bc3d7e5b96a0a412e38b2d417e93979dfcb
stan-maze <84319461+stan-maze@users.noreply.github.com>
2023-02-10 14:12:50 +0800

    Delete devmesh directory

--- devmesh/run_Sieve.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /opt/intel/oneapi/setvars.sh > /dev/null 2>&1
-/bin/echo "##" $(whoami) is compiling parallel_Sieve algorithm parallel_Sieve.cpp
-# dpcpp lab/parallel_Sieve.cpp
-icpx -fsycl lab/parallel_Sieve.cpp
-if [ $? -eq 0 ]; then ./a.out; fi
-
